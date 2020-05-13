c74074e6cf401af9095fbb6ee25769e88bcffe8b
miebz <aomieibi@yahoo.com>
2020-05-13 18:21:19 +0100

    functionality

---
+++ app.js
@@ -0,0 +1,53 @@
+//UI VARIABLES
+const btn = document.querySelector(".btn");
+
+//Load event Listeners
+eventLoader();
+
+//Load event Listeners
+function eventLoader(){
+
+    //validation of email
+    btn.addEventListener("click",validation);
+}
+
+//email validation
+
+function validation(e){
+
+    
+    //targeting warning text
+    const mail = document.querySelector(".warning");
+
+    //targeting email input
+    const email = document.querySelector(".email-input");
+
+    //getting email input value
+    const emailValue = email.value;
+
+    // //creating img for warning
+    // const img = document.createElement("img")
+
+    // img.src = "images/icon-error.svg";
+
+    // img.className = "warning-img"
+    //email Validation
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const Test = re.test(emailValue);
+    
+    
+    //Test of Validation 
+    if (Test){
+        mail.style.display = "none";
+        email.value = ""
+    }else{
+        mail.style.display = "block";
+        email.style.textAlign = "left";
+        
+    }
+
+    //clear email input
+    
+    
+    e.preventDefault();
+}
